cec5ab5366fc54145ea0aa0f95481d23c253bb58
Waga <wagaodongo@gmail.com>
2019-08-22 13:52:21 +0300

    random word

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:random_words/random_words.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  final noun = WordNoun.random();
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Word Game',
-      home: ,
+      home: Scaffold(
+          appBar: AppBar(
+            title: Text("Word Game"),
+          ),
+          body: Center(
+            child: Text('Noun: ${noun.asCapitalized}'),
+          )),
     );
   }
 }
